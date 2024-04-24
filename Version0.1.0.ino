0c330ca088aced3b221ed194d79dceaa6ebebbcd
RB2521 <156137418+RB2521@users.noreply.github.com>
2024-04-24 15:56:22 +0100

    Add files via upload

---
+++ Version0.1.0.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A0, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(8, HIGH);
+  digitalWrite(A1, HIGH);
+  digitalWrite(A2, HIGH);
+  digitalWrite(A0, HIGH);
+}
